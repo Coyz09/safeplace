93d637bdcc5f89582e9937fae805808f81c66d57
Christopher Panerio <christopherpaneriotup@gmail.com>
2023-02-25 13:58:34 +0800

    user role verification

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -203,19 +203,18 @@ class AuthController extends Controller
 
     public function get_user_info(Request $request){
         $user = User::find(Auth::user()->id);
-        // dd($user->role);
 
         if($user->role == "verified_user")
         {
             $user = DB::table('verified_users')
             ->join('users', 'verified_users.user_id',  '=', 'users.id')
-            ->select('verified_users.*','users.img')
+            ->select('verified_users.*','users.img','users.role')
             ->where('verified_users.user_id', '=',  $user->id )->get();
         }
         elseif($user->role == "unverified_user"){
         $user = DB::table('unverified_users')
             ->join('users', 'unverified_users.user_id',  '=', 'users.id')
-            ->select('unverified_users.*','users.img')
+            ->select('unverified_users.*','users.img','users.role')
             ->where('unverified_users.user_id', '=',  $user->id )->get();
         }
 
