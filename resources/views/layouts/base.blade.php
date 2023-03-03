b408579216262dce4ec76b91981ef7a74196aff2
Your Name <you@example.com>
2023-03-03 21:16:53 +0800

    signup interface update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -20,9 +20,7 @@
                 <link rel="stylesheet" type="text/css" href="{{ asset('style.css') }}">
                 <!-- <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}"> -->
                 <!-- <link rel='stylesheet' href='{{ asset('assets/css/style.css') }}' type='text/css' /> -->
-                
-                
-
+              
 
                 @yield('styles')
         </head>
@@ -38,6 +36,13 @@
         <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
+        <script src="http://code.jquery.com/jquery-3.3.1.min.js"
+                                integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+                                crossorigin="anonymous">
+                        </script>
+                        <!-- Latest compiled and minified JavaScript -->
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+                 
 
         @yield('body')
 
