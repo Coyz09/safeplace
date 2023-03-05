70718ad8232fa0261f114eb15f0c6e88c44d72cc
Christopher Panerio <christopherpaneriotup@gmail.com>
2023-03-05 14:58:11 +0800

    Update User Information API

--- routes/api.php
+++ routes/api.php
@@ -27,34 +27,38 @@ Route::get('get_user_info', 'Api\AuthController@get_user_info')->middleware('jwt
 Route::post('change_password', 'Api\AuthController@change_password')->middleware('jwtAuth');
 Route::post('update_profile_picture', 'Api\AuthController@update_profile_picture')->middleware('jwtAuth');
 
-
 Route::post('qrcode', 'Api\AuthController@qrcode');
 
 Route::post('forgot-password', 'Api\AuthController@forgot_password');
 
 
-
+//Barangay, Police Station, and Hospital
 Route::resource('hospitals', 'Api\HospitalAPIController');
 Route::resource('police_stations', 'Api\PoliceStationAPIController');
 Route::resource('barangays', 'Api\BarangayAPIController');
 
+
+//Locations
 Route::get('hospitals_location', 'Api\HospitalAPIController@location');
 Route::get('barangays_location', 'Api\BarangayAPIController@location');
 Route::get('policestations_location', 'Api\PoliceStationAPIController@location');
 
 
+//Verifications
 Route::post('verification_frontId', 'Api\VerificationController@verification_frontId')->middleware('jwtAuth');
 Route::post('verification_backId', 'Api\VerificationController@verification_backId')->middleware('jwtAuth');
 Route::post('verification_faceImage', 'Api\VerificationController@verification_faceImage')->middleware('jwtAuth');
 Route::post('verification_idDetails', 'Api\VerificationController@verification_idDetails')->middleware('jwtAuth');
 
+Route::post('update_information', 'Api\VerificationController@update_information')->middleware('jwtAuth');
 
 
 
+
+//Notifications
 Route::get('notification', 'Api\NotificationController@user_notification')->middleware('jwtAuth');
 
 Route::post('notification_read', 'Api\NotificationController@notification_read')->middleware('jwtAuth');
-
 
 Route::get('check_unread', 'Api\NotificationController@check_unread')->middleware('jwtAuth');
 
