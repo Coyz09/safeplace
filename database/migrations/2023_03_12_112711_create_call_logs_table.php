32906bbc0709c3e189102a4cb4fa22427328b18f
Christopher Panerio <christopherpaneriotup@gmail.com>
2023-03-13 19:16:01 +0800

    Implementation of Call log

---
+++ database/migrations/2023_03_12_112711_create_call_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCallLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('call_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name');
+            $table->string('name_contacted');
+            $table->string('date_contacted');
+            $table->string('time_contacted');
+            $table->string('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('call_logs');
+    }
+}
