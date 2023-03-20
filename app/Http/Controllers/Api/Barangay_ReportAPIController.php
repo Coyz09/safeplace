f9604b12aae259158f4d571b2f491631f83d38f3
Your Name <you@example.com>
2023-03-21 00:00:51 +0800

    multiple image api(updated)

--- app/Http/Controllers/Api/Barangay_ReportAPIController.php
+++ app/Http/Controllers/Api/Barangay_ReportAPIController.php
@@ -53,66 +53,77 @@ class Barangay_ReportAPIController extends Controller
         $todayTime = Carbon::now()->format('H:i:m');
         $todayDate = Carbon::now()->format('Y-m-d');
             
-        if($request->hasFile('report_images')){
-            // $images = $request->report_images;
-            // dd($request->file('report_images'));
+       if($request->hasFile('report_images')){
 
                 foreach($request->file('report_images') as $images ){
-                    $img = $images->getClientOriginalName();
-                    // $imageName =$img.'storage/images/'.time().'.jpg';
-                    $imageName =time().$img;
-                    // $input['img'] = 'storage/images/'.$img;
+                    $img = strtolower($images->getClientOriginalExtension());
+                   
+                    $random = Str::random(10);
+                    
+                    $imageName= 'storage/images/'.$random.'.'.$img; 
+                    // $imageName ='storage/images/'.time().'.jpg';
+
+
+                    // $url = $upload_path.$imageName;
+
                     $images->move(public_path('storage/images'), $imageName);
 
-                    $data[] = $imageName ;
+                    $data[] = $imageName;
                 }
-            }
-            // $img = '';
-            // if($request->report_images!=''){
-            //     foreach($request->file('report_images') as $images ){
+            } 
+//             $img = '';
+//             if($request->report_images!=''){
+//                 foreach($request->file('report_images',[]) as $images ){
            
-            //     $img = $images->getClientOriginalName();
-            //     dd($img);
-            //     $path = 'storage/images/'.time().'.jpg';
-            //     // $imageName =time().$img;
-            //     file_put_contents($path,base64_decode($img));
+//                 $img = $images->getClientOriginalName();
+                
+//                 $path = $img .time().'.jpg';
+//                 $imageName =time().$img;
+                
+//                 // file_put_contents($path,base64_decode($imageName));
+//                 $file = base64_decode($images);
+// dd($request->file('report_images'));
+//                 file_put_contents(public_path().'/storage/images/'.$path,  $file);
 
-            //  }
-            // }
-        
-        // //Report Details
-        // $barangayreport = new BarangayReports; 
-        // $barangayreport ->barangay= $request->barangay;
-        // $barangayreport ->street= $request->street;
-        // $barangayreport ->report_details= $request->report_details;
+//              }
+//             }
+
+
+
+        //Report Details
+        $barangayreport = new BarangayReports; 
+        $barangayreport ->barangay= $request->barangay;
+        $barangayreport ->street= $request->street;
+        $barangayreport ->report_details= $request->report_details;
         // $barangayreport ->report_images= json_encode($data,JSON_UNESCAPED_SLASHES);
-        // $barangayreport ->report_status= "Pending";
-        // $barangayreport ->incident_type= $request->incident_type;
+        $barangayreport ->report_images= implode('|',$data);
+        $barangayreport ->report_status= "Pending";
+        $barangayreport ->incident_type= $request->incident_type;
 
-        // // //Reported Date and time
-        // $barangayreport  ->date_reported= $todayDate;
-        // $barangayreport  ->time_reported= $todayTime;
-        // $barangayreport  ->year_reported= "2023";
-        // $barangayreport  ->date_commited= $request->date_commited;
-        // $barangayreport  ->time_commited= $request->time_commited;
+        // //Reported Date and time
+        $barangayreport  ->date_reported= $todayDate;
+        $barangayreport  ->time_reported= $todayTime;
+        $barangayreport  ->year_reported= "2023";
+        $barangayreport  ->date_commited= $request->date_commited;
+        $barangayreport  ->time_commited= $request->time_commited;
 
-        // // //Complainant Details
-        // $barangayreport  ->complainant_id= $userdetails->id;
-        // $barangayreport  ->complainant_name= $user->name;
-        // $barangayreport  ->complainant_address= $userdetails->address;
-        // $barangayreport  ->complainant_gender= $userdetails->gender;
-        // $barangayreport  ->complainant_age= $age;
-        // $barangayreport  ->complainant_contact= $userdetails->contact;
-        // $barangayreport  ->complainant_email= $userdetails->email;
-        // $barangayreport  ->complainant_identity= $request->complainant_identity;
-        // $barangayreport ->save();
+        // //Complainant Details
+        $barangayreport  ->complainant_id= $userdetails->id;
+        $barangayreport  ->complainant_name= $user->name;
+        $barangayreport  ->complainant_address= $userdetails->address;
+        $barangayreport  ->complainant_gender= $userdetails->gender;
+        $barangayreport  ->complainant_age= $age;
+        $barangayreport  ->complainant_contact= $userdetails->contact;
+        $barangayreport  ->complainant_email= $userdetails->email;
+        $barangayreport  ->complainant_identity= $request->complainant_identity;
+        $barangayreport ->save();
 
 
         return response()->json([
             'success' => true, 
-            // 'report' => $barangayreport,
-            // 'user' => $user,  
-            // 'userdetails'=>  $userdetails,
+            'report' => $barangayreport,
+            'user' => $user,  
+            'userdetails'=>  $userdetails,
             // 'report_images'=> $imageName,
            
         ]);
